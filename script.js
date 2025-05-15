5343f1077b60da295de41818fdf6ff2faf818c96
andreap <andreasofiapais@gmail.com>
2025-05-15 19:35:17 +0200

    Generar_Testimonios

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    const $testimonios = $(".cuerpo_testimonio");
+
+    function generar(){
+        generar.empty();
+        $.getJSON("https://randomuser.me/api/?results=4", function (data) {
+    
+            $.each(data.results, function (index, usuario) {
+                const testimonio = `
+                <div class="testimonio">
+                    <div class="foto1">
+                        <img src="${usuario.picture.medium}>
+                    </div>
+                    <div class="comilla">“</div>
+                    <p class="textos_t">Responden rápidamente y te aconsejan la mejor solución, cuando se te presenta cualquier tipo de duda por pequeña que esta sea.</p>
+                    <p class="nombres_t">${usuario.name.first}</p>
+                </div>`
+                
+                $testimonios.append(testimonio);
+            
+            })
+
+        }).fail(function (error) {
+        // Manejar errores de la petición y mostrar error
+        console.log("Error de carga de testimonios");
+        })
+    }
+
+    generar();
+
+    $(".cuerpo_testimonio").on(click,generar);
+
+})
